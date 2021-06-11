4145d7c1f92e848188ee9b9ca13c7363e146584d
binluo <yuripa1018@gmail.com>
2021-06-11 20:45:54 +0800

    Initial Commit

---
+++ Rovski/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Rovski
+//
+//  Created by 罗斌 on 2021/6/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
